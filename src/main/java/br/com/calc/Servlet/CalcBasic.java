0b9f052a7d26bec802a844f45ed3d926536670a0
Samir Saadi <samirsaadi@gmail.com>
2020-09-21 20:10:38 -0300

    remove commented lines

--- src/main/java/br/com/calc/Servlet/CalcBasic.java
+++ src/main/java/br/com/calc/Servlet/CalcBasic.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.calc.Servlet;
 
 import java.io.IOException;
@@ -27,14 +22,6 @@ public class CalcBasic extends HttpServlet {
         visitas = 1;
     }
 
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -66,28 +53,4 @@ public class CalcBasic extends HttpServlet {
         request.getRequestDispatcher("resultado.jsp").forward(request, response);
         visitas++;
     }
-//
-//    /**
-//     * Handles the HTTP <code>POST</code> method.
-//     *
-//     * @param request servlet request
-//     * @param response servlet response
-//     * @throws ServletException if a servlet-specific error occurs
-//     * @throws IOException if an I/O error occurs
-//     */
-//    @Override
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//    }
-//
-//    /**
-//     * Returns a short description of the servlet.
-//     *
-//     * @return a String containing servlet description
-//     */
-//    @Override
-//    public String getServletInfo() {
-//        return "Short description";
-//    }// </editor-fold>
-
 }
